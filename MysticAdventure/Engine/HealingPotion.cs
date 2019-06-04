807f51d87f958d489fb3e949628e59868378ae47
mlacanna <50681553+mlacanna@users.noreply.github.com>
2019-06-04 13:59:56 -0400

    Add files via upload
    I am not taking full credit for creating this program. There was a tutorial by Scott Lilly online that I followed. This helped me learn the basics of C#

---
+++ MysticAdventure/Engine/HealingPotion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class HealingPotion : Item //healing pot is from the base class of item
+    {
+        //also includes id and name from the item class
+        public int AmountToHeal { get; set; }
+
+        public HealingPotion(int id, string name, string namePlural, int amountToHeal) : base(id, name, namePlural) //base is what it is grabbing from the base class
+        {
+            AmountToHeal = AmountToHeal;
+        }
+    }
+}
---
+++ MysticAdventure/Engine/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Item //base class for every item
+    {
+        public int ID { get; set; } //sets the id, in all the other classes
+        public string Name { get; set; } //giving the items a name
+        public string NamePlural { get; set; }
+
+        public Item(int id, string name, string namePlural)
+        {
+            ID = id;
+            Name = name;
+            NamePlural = namePlural;
+        }
+    }
+}
